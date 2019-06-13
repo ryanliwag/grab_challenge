9acdb70e750a16d5421d9673303330b3fd508e28
Ryan <rjhontomin@gmail.com>
2019-06-13 11:38:56 +0800

    Added Imbalance dataset sampler

--- README.md
+++ README.md
@@ -18,3 +18,16 @@ Grab challenge submission for Image Classification
 
 08/06/2018 (Baseline on Simple and Complex Models)
 08/06/2018 (Try Training Simple Hard parameter sharing MTL)
+
+
+#Running in Microsoft azure ML:
+1.Create workspace (https://docs.microsoft.com/en-us/azure/machine-learning/service/quickstart-run-cloud-notebook)
+
+2.
+
+
+# 1st Approach (Transfer Learning with Complex and Simple Models)
+
+# 2nd Approach (Multi Task Learning)
+
+# 3rd Approach (Solve)
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
 from scipy.io import loadmat
+from sklearn import preprocessing
+import re
 import os
 
 def RGB_check(filepath):
@@ -15,14 +17,15 @@ def RGB_check(filepath):
 
     return False
 
-def Load_Images(root_dir, annotations_path, seed, train_split = .8, 
-                validation_split = .15, dataset_shuffle=True):
+def Load_Images(root_dir, annotations_path, 
+                seed, train_split = .8, 
+                dataset_shuffle=True):
     '''
     Take annotations, files, and metafdata
 
     return a dictionary with values.
 
-    split to train and val and test    
+    split to train and val and test
     '''
     annotations = loadmat(annotations_path)
 
@@ -34,12 +37,75 @@ def Load_Images(root_dir, annotations_path, seed, train_split = .8,
 
     nb_samples = len(file_names)
     t_idx = int(nb_samples * train_split)
-    v_idx = int(nb_samples * validation_split)
 
     train_samples = file_names[:t_idx]
-    validation_samples = file_names[t_idx:-v_idx]
-    test_samples = file_names[-v_idx:]
+    validation_samples = file_names[t_idx:]
 
-    dataset = {"training":train_samples, "validation":validation_samples,"test":test_samples}
+    dataset = {"training":train_samples, "validation":validation_samples}
 
     return dataset
+
+
+def Label_encoder(labels):
+
+    fitter = preprocessing.LabelEncoder()
+    encoded_values = fitter.fit_transform(labels)
+
+    return encoded_values
+
+
+def get_Year(car_classes):
+
+    #get Year
+    carYear = [car.split()[-1] for car in car_classes]
+    carYear_ID = Label_encoder(carYear)
+
+    return carYear, carYear_ID
+
+
+def get_Type(car_classes):
+
+    car_types = ["Coupe", "Sedan", "Cab",
+                 "Beetle", "SUV", "Van",
+                 "SuperCab", "Convertible",
+                 "Minivan", "Hatchback",
+                 "Wagon"]
+    
+    carType = []
+    for cars in car_classes:
+        isclass = False
+        for type_ in car_types:
+            
+            #List is arranged so that more generalized types are seen first
+            if re.search(r"\b{}\b".format(type_), cars, re.IGNORECASE):
+                isclass = True
+                carType.append(type_)
+                break
+        
+        if not isclass:
+            carType.append("no_type")
+
+    carType_ID = Label_encoder(carType)
+
+    return carType, carType_ID
+
+def get_Maker(car_classes):
+
+    makers = ["AM General Hummer", "Acura", "Aston Martin", "Audi" , "BMW", "Bentley", "Bugatti Veyron 16.4","Buick","Cadillac",
+             "Chevrolet", "Chrysler", "Daewoo", "Dodge", "Eagle Talon", "FIAT", "Ferrari", "Ford", "GMC", "Geo Metro Convertible", 
+             "HUMMER", "Honda", "Hyundai", "Infiniti", "Isuzu Ascender", "Jaguar", "Jeep", "Lamborghini", "Land Rover", 
+             "Lincoln Town Car", "MINI Cooper Roadster", "Maybach Landaulet", "Mazda Tribute", "McLaren MP4-12C", "Mercedes-Benz", 
+             "Mitsubishi Lancer","Nissan", "Plymouth Neon", "Porsche Panamera", "Ram C/V", "Rolls-Royce", "Scion", "Spyker", "Suzuki", 
+             "Tesla", "Toyota", "Volkswagen", "Volvo", "smart fortwo", "Fisker Karma Sedan"]
+
+    carMaker = []
+    for cars in car_classes:
+        istype = False
+        for maker in makers:
+            if re.search(r"\b{}\b".format(maker), cars):
+                istype = True
+                carMaker.append(maker)
+
+    carMaker_ID = Label_encoder(carMaker)
+
+    return carMaker, carMaker_ID
